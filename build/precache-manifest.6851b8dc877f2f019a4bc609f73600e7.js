15408231831d322317c4a345b8d6bd8709003d00
Junior Dallagnolo <ernilodjr@gmail.com>
2018-10-11 12:22:05 -0400

    Testing react-build again

---
+++ build/precache-manifest.6851b8dc877f2f019a4bc609f73600e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d17d487db0daae9528a",
+    "url": "/FirstFire/static/js/runtime~main.4d17d487.js"
+  },
+  {
+    "revision": "b8f305e558dd45e6da1b",
+    "url": "/FirstFire/static/js/main.b8f305e5.chunk.js"
+  },
+  {
+    "revision": "22ab425cd02b170badd9",
+    "url": "/FirstFire/static/js/1.22ab425c.chunk.js"
+  },
+  {
+    "revision": "b8f305e558dd45e6da1b",
+    "url": "/FirstFire/static/css/main.23b17aa5.chunk.css"
+  },
+  {
+    "revision": "22ab425cd02b170badd9",
+    "url": "/FirstFire/static/css/1.a351d702.chunk.css"
+  },
+  {
+    "revision": "f139fbef2b456de4ed3176a1cd863ff0",
+    "url": "/FirstFire/index.html"
+  }
+];
